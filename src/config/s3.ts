71d27d12591ce7b3033b09acdb1f83e5ce01add7
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2022-07-22 00:31:18 +0530

    docs(repo): JSDoc descriptions for the s3 helper fns defined

--- src/config/s3.ts
+++ src/config/s3.ts
@@ -15,17 +15,17 @@ export const s3 = new S3({
 });
 
 /**
- * @desc Makes a key that is unique to the user and the file name
+ * Makes a `Key` that is scopes to the user, followed by the filename.
  * @param userId the user id to get the file for
- * @param storageKey the file name
- * @returns a unique key
+ * @param fileName the file name
+ * @returns a unique S3 compatible key
  */
-export function s3FormStorageKey(userId: string, storageKey: string) {
-	return `${userId}/${storageKey}`;
+export function s3FormStorageKey(userId: string, fileName: string) {
+	return `${userId}/${fileName}`;
 }
 
 /**
- * @desc Makes a unique key to upload to S3
+ * Makes a unique `Key`, with a randomized filename, to then use to upload to S3.
  * @param userId the user id to get the file for
  * @param extension extension of the filename
  * @returns a new storage key
@@ -37,10 +37,10 @@ export async function makeUploadStorageKey(userId: string, extension: string) {
 }
 
 /**
- * @desc A helper function to upload a file from express & multer to S3
+ * Takes the user's `id` and a file from `express` & `multer` and upload's it onto S3.
  * @param userId
  * @param file
- * @returns the upload information such as the Tag and the Key
+ * @returns the upload information such as the Tag and the Key.
  */
 export async function uploadS3File(userId: string, file: Express.Multer.File) {
 	const fileStream = fs.createReadStream(file.path);
@@ -56,7 +56,7 @@ export async function uploadS3File(userId: string, file: Express.Multer.File) {
 }
 
 /**
- * @desc A helper function to get a file stream from S3 as a readable stream
+ * Takes a S3 storage `Key` to then query and return the file as a readable stream.
  * @param storageKey
  * @returns readable stream of the file in S3
  */
@@ -65,7 +65,9 @@ export function getS3FileStream(storageKey: string) {
 }
 
 /**
- * @desc Used to generate a presigned URL for a user on the client-side to upload an expected image.
+ * Takes the user's `id` and the `extension` of file being uploaded (eg: `png`), to then generate a presigned url to which the user may send a `PUT` request with the `Content-Type: multipart/form-data` header to upload their file.
+ *
+ * This link will be valid for 60 seconds.
  * @param userId
  * @param extension
  * @returns Presigned upload capable URL
@@ -79,17 +81,25 @@ export async function getS3PresignedUploadUrl(userId: string, extension: string)
 }
 
 /**
- * @desc Used to generate a presigned URL for a user on the client-side to download an expected image.
+ * Takes the S3 `Key` to query the bucket to generate a safe url to which the client may access the file.
+ *
+ * This is used when the bucket is NOT public, and as such requires permission to access the objects within them.
+ *
+ * A use case for this approach would be protect the document of users, so that they cannot be accessed by anyone else. As such, when authorized, the client can access the file using this link.
  * @param storageKey
  * @returns Presigned download capable URL
  */
 export async function getS3PresignedViewUrl(storageKey: string) {
 	return {
 		storageKey: storageKey,
-		url: await s3.getSignedUrlPromise("putObject", { Bucket: env.AWS_BUCKET_NAME, Key: storageKey }),
+		url: await s3.getSignedUrlPromise("getObject", { Bucket: env.AWS_BUCKET_NAME, Key: storageKey }),
 	};
 }
 
+/**
+ * Takes the S3 `Key` to delete a file stored in the bucket.
+ * @param storageKey
+ */
 export async function deleteS3Object(storageKey: string) {
 	return await s3.deleteObject({ Bucket: env.AWS_BUCKET_NAME, Key: storageKey }).promise();
 }
